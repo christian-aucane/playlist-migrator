24c6a75dafe0cdb107bb5311c33e07e15eef0ca6
Christian Aucane <christianaucane1@gmail.com>
2024-11-25 13:40:07 +0100

    Add CustomUserAdmin

--- src/authenticate/admin.py
+++ src/authenticate/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import CustomUser
+
+
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    list_filter = ('is_staff',)
+    search_fields = ('username', 'email')
