1136db36f55f4ea6f1e42fece24a3dfab10772cc
Christian Aucane <christianaucane1@gmail.com>
2024-11-25 13:45:07 +0100

    add OAuthTokenAdmin

--- src/platforms/admin.py
+++ src/platforms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import OAuthToken
+
+
+@admin.register(OAuthToken)
 # Register your models here.
+class OAuthTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'platform', 'access_token', 'expires_at', 'created_at', 'updated_at')
+    list_filter = ('user', 'platform')
+    search_fields = ('user', 'platform')
+    readonly_fields = ('user', 'platform', 'access_token', 'refresh_token', 'expires_at', 'created_at', 'updated_at')
+    fields = ('user', 'platform', 'access_token', 'refresh_token', 'expires_at', 'created_at', 'updated_at')
