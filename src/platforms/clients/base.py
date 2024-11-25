b699aef565f44584475ca6e52c58c65a506c0daa
Christian Aucane <christianaucane1@gmail.com>
2024-11-25 13:44:29 +0100

    Modify __init__ parametersÃ

--- src/platforms/clients/base.py
+++ src/platforms/clients/base.py
@@ -5,12 +5,9 @@ class BaseClient(ABC):
     """
     Base class for all APIs clients
     """
-    def __init__(self, client_id=None, client_secret=None, redirect_uri=None, scopes=None):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.redirect_uri = redirect_uri
-        self.scopes = scopes
+    def __init__(self):
         self._token_data = None
+        self._client = None
 
     @abstractmethod
     def get_auth_url(self):
@@ -42,3 +39,19 @@ class BaseClient(ABC):
         if self._token_data is None:
             raise ValueError("You need to authenticate the client first. Call authenticate_with_code().")
         return self._token_data
+
+    @token_data.setter
+    def token_data(self, token_data):
+        """
+        Sets the token data
+
+        Parameters
+        token_data: A dictionary with the access token and other information
+           {"access_token": "your_access_token",
+            "refresh_token": "your_refresh_token",
+            "expires_at": "expires_at_datetime_aware",
+            "scope": "your_scope"}
+        """
+        # TODO : ajouter vérification de la forme du dictionnaire et de la validité des informations
+        # TODO : basculer ici la transformation de la date (naive vers aware ou timestamp vers aware)
+        self._token_data = token_data
--- src/platforms/clients/youtube.py
+++ src/platforms/clients/youtube.py
@@ -7,17 +7,18 @@ from .base import BaseClient
 
 class YoutubeClient(BaseClient):
     def __init__(self, client_secrets_file, scopes, redirect_uri=None):
-        super().__init__(scopes=scopes,
-                         redirect_uri=redirect_uri)
+        super().__init__()
         self.flow = InstalledAppFlow.from_client_secrets_file(
             client_secrets_file,
             scopes,
             redirect_uri=redirect_uri
         )
         self.credentials = None
-        self.youtube = None
 
     def get_auth_url(self):
+        """
+        Returns the authorization URL for the client.
+        """
         return self.flow.authorization_url()[0]
 
     def authenticate_with_code(self, code):
@@ -31,30 +32,13 @@ class YoutubeClient(BaseClient):
         self.flow.fetch_token(code=code)
         self.credentials = self.flow.credentials
 
-        expires_at = None
-        if self.credentials.expiry:
-            expires_at = self.credentials.expiry  # datetime naive (without timezone information)
-
-            # Convert to datetime aware (with timezone information)
-            expires_at = timezone.make_aware(expires_at)
-
-        self._token_data = {
+        self.token_data = {
             'access_token': self.credentials.token,
             'refresh_token': self.credentials.refresh_token,
-            'expires_at': expires_at,
+            'expires_at': timezone.make_aware(self.credentials.expiry) if self.credentials.expiry else None,  # datetime aware (with timezone
+            # information)
             'scope': " ".join(self.credentials.scopes) if self.credentials.scopes else None,
         }
 
         # Instantiate the YouTube service client
-        self.youtube = build("youtube", "v3", credentials=self.credentials)
-
-
-
-if __name__ == "__main__":
-    client = YoutubeClient(
-        client_secrets_file="../../../client_secrets.json",
-        scopes=["https://www.googleapis.com/auth/youtube.readonly"],
-        redirect_uri="http://localhost:8080/callback"
-    )
-
-    print(client.get_auth_url())
+        self._client = build("youtube", "v3", credentials=self.credentials)
