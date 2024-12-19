1d3ceb1ce18e59af3bd76a4fb8af6a1e48348ea8
Christian Aucane <christianaucane1@gmail.com>
2024-12-19 14:07:01 +0100

    Add route and link to UserTrackClearView

--- src/main_app/urls.py
+++ src/main_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AppIndexView, UpdateSavedTracksView, UserTrackTableView, UserTrackDeleteView
+from .views import AppIndexView, UpdateSavedTracksView, UserTrackTableView, UserTrackDeleteView, UserTrackClearView
 
 app_name = "main_app"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("update_saved_tracks/<str:platform>/", UpdateSavedTracksView.as_view(), name="update_saved_tracks"),
     path("tracks/", UserTrackTableView.as_view(), name="user_track_table"),
     path("tracks/<int:pk>/delete/", UserTrackDeleteView.as_view(), name="user_track_delete"),
+    path("tracks/clear/", UserTrackClearView.as_view(), name="user_track_clear"),
 ]
