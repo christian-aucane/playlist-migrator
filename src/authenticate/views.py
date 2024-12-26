4b77d48453054dc3975c9de6a85edfd6d58a4b59
Christian Aucane <christianaucane1@gmail.com>
2024-12-26 20:38:41 +0100

    Add SignupView

--- src/authenticate/views.py
+++ src/authenticate/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import TemplateView, CreateView
+
+from authenticate.forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 class CustomLoginView(LoginView):
@@ -16,5 +21,8 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('home')
 
 
-class SignupView(TemplateView):
-    template_name = "core/home"  # TODO : modifier
+class SignupView(CreateView):
+    model = User
+    form_class = CustomUserCreationForm
+    template_name = "authenticate/signup.html"
+    success_url = reverse_lazy("authenticate:login")
